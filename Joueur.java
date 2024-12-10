a743df3641ac86dccc7650cae5f999d3d1a49bdb
Clemence Bocquet <143797406+clemencebc1@users.noreply.github.com>
2024-12-10 17:40:12 +0100

    Add files via upload

---
+++ Joueur.java
@@ -0,0 +1,21 @@
+
+class Joueur {
+  public String getCouleur() {
+  }
+
+  private False gameOver = boolean;
+
+  private String couleur;
+
+  public Joueur(String couleur) {
+  }
+
+  public boolean aGagne() {
+  }
+
+  public void reset() {
+  }
+
+  private Plateau lePlateau;
+
+}
