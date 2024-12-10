8eeb07f8e631ab15bce4d376fd4c40a50e300412
Naima Akhtar <naimasanaakhtar@gmail.com>
2024-12-10 18:20:44 +0100

    modifications case.java

--- Case.java
+++ Case.java
@@ -1,21 +1,27 @@
+import java.util.ArrayList;
+import java.util.List;
 
-class Case {
-  private boolean aJeton = False;
-
-  public Case() {
-  }
-
-  public void reset() {
-  }
-
-  public boolean contientJeton() {
-  }
-
-  public boolean poseJeton() {
-  }
-
-  private <Case> lesVoisines;
-
+public class Case {
+  private boolean aJeton = false;
   private String couleur = null;
+  private List<Case> lesVoisines;
 
+  public Case(){
+    this.lesVoisines = new ArrayList<>();
+  }
+
+  public void reset(){
+    this.aJeton = false;
+    this.couleur = null;
+  }
+
+  public boolean contientJeton(){
+    return this.aJeton;
+  }
+
+  public boolean poseJeton(String couleur){
+    this.aJeton = true;
+    this.couleur = couleur;
+    return true;
+  }
 }
