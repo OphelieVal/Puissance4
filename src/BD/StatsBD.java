c833a6667cdff4fdf31e48b8de2134eaa19927e4
Naima Akhtar <naimasanaakhtar@gmail.com>
2025-01-14 18:34:56 +0100

    début du codage de StatsBD

--- src/BD/StatsBD.java
+++ src/BD/StatsBD.java
@@ -1,6 +1,25 @@
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+
+import modele.Joueur;
+import modele.Stats;
+
 public class StatsBD {
     private ConnexionMySQL connexion;
 
+    public StatsBD(){
+        this.connexion = new ConnexionMySQL("servinfo-maria", "DBakhtar", "akhtar", "noalecaca");
+    }
+
+    public Stats getStatsparJoueur(Joueur joueur){
+        try{
+            Connection c = this.connexion.getConnexion();
+            String requete = "SELECT NbVictoires, NbParties FROM STATS WHERE idp = ?";
+            PreparedStatement ps = c.prepareStatement(requete);
+            ps.setInt(1, joueur.getId())
+        }
+    }
 }
+
 
 
