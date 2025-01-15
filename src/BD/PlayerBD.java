fb79a43b2f38f050e3da4db772864a0686dd2848
Naima <naimasanaakhtar@gmail.com>
2025-01-15 07:13:56 +0100

    ajout de méthodes dans la classe PlayerBD

--- src/BD/PlayerBD.java
+++ src/BD/PlayerBD.java
@@ -10,7 +10,7 @@ public class PlayerBD {
         this.connexion = new ConnexionMySQL("servinfo-maria", "DBakhtar", "akhtar", "noalecaca");
     }
     
-    public List<Joueur> getListePersonne(){
+    public List<Joueur> getListeJoueurs(){
         List<modele.Joueur> res = new ArrayList<>();
         try{
             Connection c = this.connexion.getConnexion();
@@ -30,5 +30,39 @@ public class PlayerBD {
             System.out.println("erreur :" + e.getMessage());
         }
         return res;
-    } 
+    }
+
+    public List<Joueur> getListeJoueursEnLigne() {
+        List<Joueur> res = new ArrayList<>();
+        try {
+            Connection c = this.connexion.getConnexion();
+            Statement s = c.createStatement();
+            ResultSet rs = s.executeQuery("SELECT idp, nomp, couleurp FROM PLAYER WHERE connecte = true");
+            int idp;
+            String nomp, couleurp;
+            while (rs.next()) {
+                idp = rs.getInt("idp");
+                nomp = rs.getString("nomp");
+                couleurp = rs.getString("couleurp");
+                Joueur joueur = new Joueur(nomp, couleurp, idp);
+                res.add(joueur);
+            }
+            rs.close();
+        } catch (SQLException e) {
+            System.out.println("erreur : " + e.getMessage());
+        }
+        return res;
+    }
+    
+    public void setJoueurConnecte(int idJoueur, boolean connecte) {
+        try {
+            Connection c = this.connexion.getConnexion();
+            PreparedStatement ps = c.prepareStatement("UPDATE PLAYER SET connecte =? WHERE idp =?");
+            ps.setBoolean(1, connecte);
+            ps.setInt(2, idJoueur);
+            ps.executeUpdate();
+        } catch (SQLException e) {
+            System.out.println("erreur : " + e.getMessage());
+        }
+    }
 }
